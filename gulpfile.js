f3bd50c200c1ca4c92129da90c0dbfc09e64e4e4
arestch <invfalle@gmail.com>
2017-12-11 13:52:52 +0300

    added uglify for js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 "use strict";
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var rename = require("gulp-rename");
-var minify = require("gulp-minify");
-var cleanCSS = require('gulp-clean-css');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    plumber = require("gulp-plumber"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    server = require("browser-sync").create(),
+    rename = require("gulp-rename"),
+    minify = require("gulp-minify"),
+    cleanCSS = require('gulp-clean-css'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    concat = require('gulp-concat');
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -25,6 +29,25 @@ gulp.task("style", function() {
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
+   
+gulp.task('compress', function() {
+  return gulp.src([
+    'scripts/common.js',
+    ])
+  .pipe(concat('common.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('js'));
+});
+
+
+gulp.task('js', ['compress'], () =>
+    gulp.src('js/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('scripts'))
+);
+
 
 gulp.task('browser-sync', function () {
         browserSync({
@@ -38,7 +61,7 @@ gulp.task('browser-sync', function () {
 });
 
 
-gulp.task("serve", ["style"], function() {
+gulp.task('serve', ['style', 'compress'], function() {
   server.init({
     server: ".",
     notify: false,
@@ -48,5 +71,6 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("js/*.js", ["js"]);
   gulp.watch("*.html").on("change", server.reload);
 });
